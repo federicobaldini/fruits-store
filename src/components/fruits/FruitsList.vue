601fa92e3751bd8f9db111d8a9b0dd0745f78527
federicobaldini <federico-baldini@hotmail.com>
2022-03-27 18:58:27 +0200

    added new FruitsList component

---
+++ src/components/fruits/FruitsList.vue
@@ -0,0 +1,20 @@
+<script>
+import FruitsItem from "./FruitsItem.vue";
+
+export default {
+  name: "FruitsList",
+  data() {
+    return {};
+  },
+  components: { FruitsItem },
+};
+</script>
+
+<template>
+  <div class="fruits-list">
+    <div>FruitsList Works!</div>
+    <FruitsItem />
+  </div>
+</template>
+
+<style scoped></style>
