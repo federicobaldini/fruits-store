74ba63cabe280436dd117b04370295bddac69a8c
federicobaldini <federico-baldini@hotmail.com>
2022-03-27 18:46:14 +0200

    first commit

---
+++ src/views/FruitsView.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      text: "FruitsView Works!",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="fruits">{{ this.text }}</div>
+</template>
+
+<style scoped></style>
