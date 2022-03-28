cd7d7367730293dc90d313b379ed43cc28bbf23a
federicobaldini <federico-baldini@hotmail.com>
2022-03-28 09:56:27 +0200

    added new SearchBar component

---
+++ src/components/search/SearchBar.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return { inputText: "" };
+  },
+};
+</script>
+
+<template>
+  <div class="searchbar">SearchBar Works!</div>
+</template>
+
+<style scoped></style>
