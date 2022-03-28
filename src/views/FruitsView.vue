6ecfd7bc4da8f570089f2d7294bf86add2194132
federicobaldini <federico-baldini@hotmail.com>
2022-03-28 09:57:21 +0200

    added fruits data

--- src/views/FruitsView.vue
+++ src/views/FruitsView.vue
@@ -3,14 +3,29 @@ import FruitsList from "../components/fruits/FruitsList.vue";
 
 export default {
   data() {
-    return {};
+    return {
+      fruits: [
+        {
+          id: 1,
+          name: "watermelon",
+          imagePath:
+            "https://www.freepnglogos.com/uploads/watermelon-png/watermelon-png-watermelon-png-image-download-36.png",
+        },
+        {
+          id: 2,
+          name: "avocado",
+          imagePath:
+            "https://www.pngall.com/wp-content/uploads/2016/05/Avocado-PNG.png",
+        },
+      ],
+    };
   },
   components: { FruitsList },
 };
 </script>
 
 <template>
-  <div class="fruits"><FruitsList /></div>
+  <div class="fruits"><FruitsList :fruits="fruits" /></div>
 </template>
 
 <style scoped></style>
