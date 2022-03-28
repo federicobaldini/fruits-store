ac1d61e92563c9627af51a45137b35de9f499c98
federicobaldini <federico-baldini@hotmail.com>
2022-03-28 09:56:48 +0200

    improved FruitsItem component

--- src/components/fruits/FruitsItem.vue
+++ src/components/fruits/FruitsItem.vue
@@ -1,6 +1,8 @@
 <script>
 export default {
   name: "FruitsItem",
+  props: ["fruit"],
+
   data() {
     return {};
   },
@@ -8,7 +10,39 @@ export default {
 </script>
 
 <template>
-  <div class="fruits-item">FruitsItem Works!</div>
+  <div class="fruits-item">
+    <h1 class="fruits-item__title">{{ this.fruit.name.toUpperCase() }}</h1>
+    <div class="fruits-item__image-container">
+      <img class="fruits-item__image" :src="this.fruit.imagePath" alt="fruit" />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.fruits-item {
+  position: relative;
+  margin: 0;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 6px;
+  padding: 1rem;
+  margin: 1rem;
+  overflow: hidden;
+  background: white;
+  height: 15rem;
+  width: 15rem;
+}
+
+.fruits-item__title {
+  font-size: 18px;
+}
+
+.fruits-item__image-container {
+  display: flex;
+  justify-content: center;
+  height: 80%;
+}
+
+.fruits-item__image {
+  padding: 1rem;
+}
+</style>
